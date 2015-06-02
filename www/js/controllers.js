ca2d0939f2a3026f85a13f05d43775d513d5cedd
Connor Leech <connorleech@gmail.com>
2015-06-02 10:25:55 -0700

    redirect after login`

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,18 +10,21 @@ app.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
   
 })
 
-.controller('AuthCtrl', function($scope, $firebaseAuth, $cordovaOauth){
+.controller('AuthCtrl', function($scope, $rootScope, $firebaseAuth, $cordovaOauth, $state){
   var fb_app_id = '1416319645359452';
   var ref = new Firebase("http://giphy.firebaseio.com/");
   var auth = $firebaseAuth(ref);
-  $scope.user = null;
+  $rootScope.user = null;
 
   $scope.login = function() {
     $cordovaOauth.facebook(fb_app_id, ["email"]).then(function(result) {
         auth.$authWithOAuthToken("facebook", result.access_token).then(function(authData) {
             console.log(JSON.stringify(authData));
-            $scope.user = authData;
-            $scope.$apply();
+            
+            $rootScope.user = authData;
+            $rootScope.$apply();
+            $state.go('app.browse');
+
         }, function(error) {
             console.error("ERROR: " + error);
         });
